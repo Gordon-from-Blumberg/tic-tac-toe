73f37e4b324d5ea63e939c75211e1c7ac16c2637
Gordon from Blumberg <gordon.from.blumberg@gmail.com>
2021-03-23 03:09:15 +0400

    Create MovingStrategy interface and some implementations

--- core/src/com/gordonfromblumberg/games/common/model/PhysicsGameObject.java
+++ core/src/com/gordonfromblumberg/games/common/model/PhysicsGameObject.java
@@ -1,18 +1,13 @@
 package com.gordonfromblumberg.games.common.model;
 
 import com.badlogic.gdx.math.Vector2;
+import com.gordonfromblumberg.games.common.physics.MovingStrategy;
 
 public class PhysicsGameObject extends GameObject {
     public final Vector2 velocity = new Vector2();
     public final Vector2 acceleration = new Vector2();
 
-    protected boolean moveToTarget = false;
-    public final Vector2 desiredMovement = new Vector2();
-    protected final Vector2 desiredVelocity = new Vector2();
-
-    protected float decelerationDistance, decelerationDistance2;
-    public float maxVelocity, maxVelocity2;
-    protected float maxAcceleration, maxDeceleration;
+    protected MovingStrategy movingStrategy;
 
     {
         this.colliding = true;
@@ -22,78 +17,11 @@ public class PhysicsGameObject extends GameObject {
     public void update(final float delta) {
         super.update(delta);
 
-        setTarget();
+        if (movingStrategy != null)
+            movingStrategy.update(position, velocity, acceleration, delta);
 
-        updateAcceleration(delta);
-
-        updateVelocity(delta);
-
-        position.mulAdd(velocity, delta);
         adjustPosition();
         polygon.setPosition(position.x, position.y);
-    }
-
-    protected void setTarget() {
-        moveToTarget = false;
-    }
-
-    protected void updateAcceleration(float delta) {
-        if (moveToTarget) {
-            Vector2 desiredMovement = this.desiredMovement;
-            Vector2 desiredVelocity = this.desiredVelocity;
-            Vector2 acceleration = this.acceleration;
-
-            float desiredMovementLen2 = desiredMovement.len2();
-            float velocityLimit2 = desiredMovementLen2 < decelerationDistance2
-                    ? desiredMovementLen2 / decelerationDistance2 * maxVelocity2
-                    : maxVelocity2;
-            desiredVelocity.set(desiredMovement).limit2(velocityLimit2);
-
-            acceleration.set(desiredVelocity).sub(velocity);
-
-            float accelerationLimit = acceleration.dot(velocity) > 0
-                    ? maxAcceleration
-                    : maxDeceleration;
-
-            acceleration.scl(1 / delta).limit2(accelerationLimit * accelerationLimit);
-        }
-    }
-
-    protected void updateVelocity(float delta) {
-        velocity.mulAdd(acceleration, delta)
-                .limit2(maxVelocity2);
-    }
-
-    public void setDecelerationDistance(float decelerationDistance) {
-        this.decelerationDistance = decelerationDistance;
-        this.decelerationDistance2 = decelerationDistance * decelerationDistance;
-    }
-
-    public void setMaxVelocity(float maxVelocity) {
-        this.maxVelocity = maxVelocity;
-        this.maxVelocity2 = maxVelocity * maxVelocity;
-
-        float calculatedDecelerationDist = calcDecelerationDist();
-        if (calculatedDecelerationDist > decelerationDistance) {
-            setDecelerationDistance(calculatedDecelerationDist);
-        }
-    }
-
-    public void setMaxAcceleration(float maxAcceleration) {
-        this.maxAcceleration = maxAcceleration;
-    }
-
-    public void setMaxDeceleration(float maxDeceleration) {
-        this.maxDeceleration = maxDeceleration;
-
-        float calculatedDecelerationDist = calcDecelerationDist();
-        if (calculatedDecelerationDist > decelerationDistance) {
-            setDecelerationDistance(calculatedDecelerationDist);
-        }
-    }
-
-    protected float calcDecelerationDist() {
-        return maxVelocity2 / (2 * maxDeceleration);
     }
 
     /**
---
+++ core/src/com/gordonfromblumberg/games/common/physics/GravityMovingStrategy.java
@@ -0,0 +1,24 @@
+package com.gordonfromblumberg.games.common.physics;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class GravityMovingStrategy extends AccelerationMovingStrategy {
+    protected final Vector2 gravity = new Vector2();
+
+    public GravityMovingStrategy() {}
+
+    public GravityMovingStrategy(float gravityX, float gravityY) {
+        setGravity(gravityX, gravityY);
+    }
+
+    @Override
+    public void update(Vector2 position, Vector2 velocity, Vector2 acceleration, float dt) {
+        velocity.mulAdd(gravity, dt);
+        super.update(position, velocity, acceleration, dt);
+    }
+
+    public void setGravity(float gravityX, float gravityY) {
+        gravity.x = gravityX;
+        gravity.y = gravityY;
+    }
+}
