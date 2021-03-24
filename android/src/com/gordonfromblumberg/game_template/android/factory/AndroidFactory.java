b785b9cf43e1739737235b6581fd1826930d5855
gordon_from_blumberg <gordon.from.blumberg@gmail.com>
2021-03-25 01:40:46 +0400

    Remove game_template package

--- android/src/com/gordonfromblumberg/game_template/android/factory/AndroidFactory.java
+++
@@ -1,9 +0,0 @@
-package com.gordonfromblumberg.game_template.android.factory;
-
-import com.gordonfromblumberg.game_template.factory.AbstractFactory;
-
-public class AndroidFactory extends AbstractFactory {
-    public AndroidFactory() {
-        AbstractFactory.instance = this;
-    }
-}
