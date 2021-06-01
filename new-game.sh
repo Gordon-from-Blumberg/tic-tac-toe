36c58c036bf97769e10c154c12e7640a0ac2b005
Gordon-from-Blumberg <gordon.from.blumberg@gmail.com>
2021-06-02 01:27:32 +0400

    Set user name and email

--- new-game.sh
+++ new-game.sh
@@ -11,3 +11,8 @@ git remote rename origin template
 
 echo "Add new remote origin with url $1"
 git remote add -f origin $1
+
+echo 'Set user name to Gordon-from-Blumberg'
+git config user.name Gordon-from-Blumberg
+echo 'Set user email to gordon.from.blumberg@gmail.com'
+git config user.email gordon.from.blumberg@gmail.com
