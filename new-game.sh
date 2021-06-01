0a130b34defd6f88efa47b5feeccd2d5589e7879
Aleksandr Ivko <AIvko@egartech.com>
2021-06-02 01:22:54 +0400

    Add check for specified url

--- new-game.sh
+++ new-game.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+    then
+        echo 'Url of a new repository not specified!'
+        exit 1
+fi
+
 echo 'Rename remote origin -> template'
 git remote rename origin template
 
