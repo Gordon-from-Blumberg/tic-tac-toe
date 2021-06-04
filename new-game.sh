82c993781927c9cbbf97802bb3ef04702eca923b
gordon_from_blumberg <gordon.from.blumberg@gmail.com>
2021-06-05 01:40:48 +0400

    Change user name to gordon

--- new-game.sh
+++ new-game.sh
@@ -12,8 +12,8 @@ git remote rename origin template
 echo "Add new remote origin with url $1"
 git remote add -f origin $1
 
-echo 'Set user name to Gordon-from-Blumberg'
-git config user.name Gordon-from-Blumberg
+echo 'Set user name to gordon'
+git config user.name gordon
 echo 'Set user email to gordon.from.blumberg@gmail.com'
 git config user.email gordon.from.blumberg@gmail.com
 echo 'Set push.default to upstream'
