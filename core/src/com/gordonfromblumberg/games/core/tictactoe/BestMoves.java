e761667a6c4854e8117673999aff251f87833591
gordon <gordon.from.blumberg@gmail.com>
2021-07-17 04:48:33 +0400

    Tic Tac Toe

--- core/src/com/gordonfromblumberg/games/core/tictactoe/BestMoves.java
+++ core/src/com/gordonfromblumberg/games/core/tictactoe/BestMoves.java
@@ -9,33 +9,74 @@ public class BestMoves {
         int state = 0;
         final IntIntMap map = new IntIntMap();
 
-        move(state, false, map);
+        oppMove(state, map);
 
         System.out.println("map size = " + map.size);
+        state = 0b000000000000000010;
+        System.out.println(stateToString(state));
+        while (map.containsKey(state)) {
+            state = map.get(state, -1);
+            System.out.println(stateToString(state));
+        }
     }
 
-    private static void move(int state, boolean myTurn, IntIntMap map) {
+    private static int myMove(int state, IntIntMap map) {
         if (map.containsKey(state))
-            return;
+            return 1;
 
-        int sign = myTurn ? MY_CELL : OPPONENT_CELL;
+        int bestMove = -1, bestResult = -1;
+        int moveCount = 0;
         for (int cell : CELL_MASK) {
             if ((cell & state) == EMPTY_CELL) {
-                int cellSign = sign & cell;
+                moveCount++;
+                int cellSign = MY_CELL & cell;
                 int newState = state | cellSign;
-                if (checkWin(newState, sign)) {
 
-                    if (myTurn) {
-                        map.put(state, newState);
-                    } else {
-                        return;
-                    }
+                if (checkWin(newState, MY_CELL)) {
+
+                    map.put(state, newState);
+                    return 1;
 
                 } else {
-                    move(newState, !myTurn, map);
+                    int result = oppMove(newState, map);
+                    if ((newState & 0b000000000100000010) == 0b000000000100000010) {
+                        System.out.println("newState:" + stateToString(newState));
+                        System.out.println("result = " + result);
+                    }
+
+                    if (result > bestResult) {
+                        bestResult = result;
+                        bestMove = newState;
+                    }
                 }
             }
         }
+
+        if (bestMove != -1)
+            map.put(state, bestMove);
+
+        return moveCount == 0 ? 0 : bestResult;
+    }
+
+    private static int oppMove(int state, IntIntMap map) {
+        for (int cell : CELL_MASK) {
+            if ((cell & state) == EMPTY_CELL) {
+                int cellSign = OPPONENT_CELL & cell;
+                int newState = state | cellSign;
+
+                if (checkWin(newState, OPPONENT_CELL)) {
+
+                    return -1;
+
+                } else {
+                    int result = myMove(newState, map);
+                    if (result == -1)
+                        return -1;
+                }
+            }
+        }
+
+        return 0;
     }
 
     private static boolean checkWin(int state, int sign) {
@@ -47,5 +88,18 @@ public class BestMoves {
             }
         }
         return false;
+    }
+
+    private static String stateToString(int state) {
+        StringBuilder sb = new StringBuilder("\n");
+        for (int i = 0, n = CELL_MASK.length; i < n; i++) {
+            if (i != 0 && i % 3 == 0)
+                sb.append("\n");
+            int cell = CELL_MASK[i] & state;
+            sb.append("|").append(cell == EMPTY_CELL ? ' ' : cell == (CELL_MASK[i] & MY_CELL) ? "o" : "x");
+            if (i % 3 == 2)
+                sb.append("|");
+        }
+        return sb.toString();
     }
 }
